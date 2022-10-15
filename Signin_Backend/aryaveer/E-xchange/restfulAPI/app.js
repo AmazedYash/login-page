cfd41b120d56a48207521394ce2d24d837b58a68
AmazedYash <115898126+AmazedYash@users.noreply.github.com>
2022-10-16 01:10:03 +0530

    changing port

--- Signin_Backend/aryaveer/E-xchange/restfulAPI/app.js
+++ Signin_Backend/aryaveer/E-xchange/restfulAPI/app.js
@@ -3,7 +3,7 @@ const logindata = require("./logindatabaseStructure");
 require("./connect");
 const loginRouter = require("./routers/loginRouter");
 const app  =  express();
-const port  =  process.env.PORT || 8000;
+const port  =  process.env.PORT || 8080;
 app.use(express.json());
 app.use(loginRouter);
 app.listen(port , ()  =>
